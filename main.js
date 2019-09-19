b53c7f43848a7bb3d6c03dc70772538a7f2418d9
Marcilio2 <marcilio.tekno@gmail.com>
2019-09-19 11:49:33 +0000

    v3

---
+++ main.js
@@ -0,0 +1,22 @@
+const ovly = { };
+
+ovly.conversor = { 
+	versao: "1",
+	chamarAPI: function(){
+		console.log("Chamando API");
+		const endpoint = "api/latest";
+		var oParametros = {
+			base: $("#moeda_origem").val()
+		};
+		$.get(endpoint, oParametros, this.callback );
+	},
+	callback: function(resultado){
+		// console.log(resultado);
+		var sMoedaDestino = $("#moeda_destino").val();
+		var fValor = resultado.rates[sMoedaDestino];
+		$("#valor_destino").text(fValor);
+		$("#moeda_destino_celula").text(sMoedaDestino);
+	}
+}
+
+© 2019 GitHub, Inc.
